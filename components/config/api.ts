34e45861ef7bfc829b45526c8da31d3323465532
Amar733 <amarkumar00702@gmail.com>
2026-07-07 09:47:06 +0530

    add

--- components/config/api.ts
+++ components/config/api.ts
@@ -1,8 +1,8 @@
 // API Configuration
 export const API_BASE_URL = 
    process.env.NEXT_PUBLIC_API_URL 
-// || 'https://demo-backend-1-uc12.onrender.com/api' 
-|| 'http://localhost:5000/api';
+|| 'https://demo-backend-1-uc12.onrender.com/api' 
+// || 'http://localhost:5000/api';
 
 // API Endpoints
 export const API_ENDPOINTS = {
